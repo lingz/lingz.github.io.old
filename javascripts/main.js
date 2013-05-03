78d2f5eb548ea42c6cdc31490f14914d91266bb2
lingz <lz781@nyu.edu>
2013-05-03 23:28:01 +0400

    minor fixes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#navbar ul li a, .brand').bind('click', function(e) {
+  $('#navbar ul li a, .section-links a').bind('click', function(e) {
    e.preventDefault();
    $('html, body').animate({ scrollTop: $(this.hash).offset().top - 60 }, 300);
   });
